09d978c07bbbdb706e9141296a1f514d4f136ea8
Davit Tabidze <davidtabidzef1@gmail.com>
2023-06-05 17:40:36 -0400

    v0.7

--- index.js
+++ index.js
@@ -177,66 +177,6 @@ function currentLocationWeather (city) {
 
 
 })
-
-
-        // const hL = document.createElement('h1')
-        // hL.textContent = 'Current Location'
-        // weatherDiv.appendChild(hL)
-        // hL.classList.add('location-title');
-        
-        // const location = document.createElement('p');
-        // location.setAttribute('id', 'location');
-        // location.textContent = `City: ${data.location.name}`;
-        // weatherDiv.appendChild(location);
-
-        // const country = document.createElement('p');
-        // country.setAttribute('id', 'country');
-        // country.textContent = `Country: ${data.location.country}`;
-        // weatherDiv.appendChild(country);
-
-        // const coordinates = document.createElement('p');
-        // coordinates.setAttribute('id', 'coordinates');
-        // coordinates.textContent = `Coordinates: lat: ${data.location.lat} lon: ${data.location.lon}`;
-        // weatherDiv.appendChild(coordinates);
-
-        // const localtime = document.createElement('p');
-        // localtime.setAttribute('id', 'localtime');
-        // localtime.textContent = `Localtime: ${data.location.localtime}`;
-        // weatherDiv.appendChild(localtime);
-
-        // const h = document.createElement('h1')
-        // h.textContent = 'Current Weather'
-        // weatherDiv.appendChild(h)
-
-        // const tempC = document.createElement('p')
-        // tempC.setAttribute('id','temp-c')
-        // tempC.textContent = `Current Temp: ${data.current.temp_c} °C`;
-        // weatherDiv.appendChild(tempC);
-        // //console.log (tempC);
-
-        // const weatherImg = document.createElement('img')
-        // const img = data.current.condition.icon.slice(2);
-        // weatherImg.src = `http://${img}`;
-        // weatherImg.classList.add('weather-icon-temp');
-        // weatherDiv.append(weatherImg);
-        // //console.log (weatherImg.src)
-
-        // const windSpeed = document.createElement('p')
-        // windSpeed.setAttribute('id','wind-speed')
-        // windSpeed.textContent = `Wind Speed: ${data.current.wind_kph} KPH`;
-        // weatherDiv.appendChild(windSpeed);
-
-        // const windDirection = document.createElement('p')
-        // windDirection.setAttribute('id','wind-direction')
-        // windDirection.textContent = `Wind Direction: ${data.current.wind_dir}`;
-        // weatherDiv.appendChild(windDirection);
-
-        // const humidity = document.createElement('p')
-        // humidity.setAttribute('id','wind-direction')
-        // humidity.textContent = `Humidity: ${data.current.humidity} %`;
-        // weatherDiv.appendChild(humidity);
-
-        // console.log(data)})
 }
 
 //init()
