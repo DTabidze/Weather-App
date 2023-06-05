908d26972538655819081c741f121fea194fd9a7
Davit Tabidze <davidtabidzef1@gmail.com>
2023-06-05 17:09:07 -0400

    v0.5

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function init () {
 
 function currentLocationWeather (city) {
     //const URL = `http://api.weatherapi.com/v1/current.json?key=476fc45bade541f8988153529230506&q=${city}&aqi=no`
-    const URL = `http://api.weatherapi.com/v1/forecast.json?key=476fc45bade541f8988153529230506&q=${city}&days=1&aqi=no&alerts=no`
+    const URL = `http://api.weatherapi.com/v1/forecast.json?key=476fc45bade541f8988153529230506&q=${city}&days=7&aqi=no&alerts=no`
     console.log(city);
     fetch (URL)
     .then (response => response.json())
@@ -37,6 +37,12 @@ function currentLocationWeather (city) {
         location.textContent = `${data.location.name}`;
         location.classList.add('city-name');
         weatherDiv.appendChild(location);
+
+        const currentTime = document.createElement('p');
+        currentTime.setAttribute('id', 'current-time');
+        currentTime.textContent = `${data.location.localtime}`;
+        currentTime.classList.add('temp-range');
+        weatherDiv.appendChild(currentTime);
         
         const weatherIconTemp = document.createElement('div');
         weatherIconTemp.classList.add('weather-icon-temp');
@@ -62,16 +68,110 @@ function currentLocationWeather (city) {
         
         const tempMinMax = document.createElement('p');
         tempMinMax.setAttribute('id', 'temp-min-max');
-        tempMinMax.textContent = `Min: ${data.forecast.forecastday[0].day.mintemp_c} °C | Max: ${data.forecast.forecastday[0].day.maxtemp_c} °C`;
+        tempMinMax.textContent = `H: ${data.forecast.forecastday[0].day.maxtemp_c} °C | L: ${data.forecast.forecastday[0].day.mintemp_c} °C`;
         tempMinMax.classList.add('temp-range');
         weatherDiv.appendChild(tempMinMax);
         
         const windInfo = document.createElement('p');
         windInfo.setAttribute('id', 'wind-info');
-        windInfo.textContent = `Wind: ${data.current.wind_kph} KPH ${data.current.wind_dir}`;
+        windInfo.textContent = `Wind: ${data.current.wind_kph} kph ${data.current.wind_dir}`;
         windInfo.classList.add('wind-info');
         weatherDiv.appendChild(windInfo);
         
+        
+        //houre forecast
+        let localTimeHoure = data.location.localtime.split(' ')[1].split(':')[0];
+        console.log (+localTimeHoure);
+        
+        //console.log (data.forecast.forecastday[0].hour[1])
+        //collect next 24 houre weather forecast
+        const forecastData = [ ];
+        console.log (data.forecast.forecastday[0].hour[16].time)
+        let j = 0;
+        for (let i = +localTimeHoure + 1; i < +localTimeHoure + 25; i++) {
+            if (i < 24) {
+                console.log(i)
+                const houreItem = {
+                    time: data.forecast.forecastday[0].hour[i].time.split(' ')[1],
+                    temp_c: data.forecast.forecastday[0].hour[i].temp_c,
+                    img: data.forecast.forecastday[0].hour[i].condition.icon
+                }
+                forecastData.push(houreItem)
+            } else {
+                //console.log(i);
+                const houreItemNextDay = {
+                    time: data.forecast.forecastday[1].hour[j].time.split(' ')[1],
+                    temp_c: data.forecast.forecastday[1].hour[j].temp_c,
+                    img: data.forecast.forecastday[1].hour[j].condition.icon
+                }
+                forecastData.push(houreItemNextDay)
+                j++
+            }
+            
+        }
+
+        console.log (forecastData)
+        const forecastContainer = document.createElement('div');
+        forecastContainer.classList.add('hourly-forecast');
+        document.body.appendChild(forecastContainer);
+
+        let isMouseDown = false;
+        let startX = 0;
+        let scrollLeft = 0;
+
+        // handle mouse down event
+        forecastContainer.addEventListener('mousedown', (e) => {
+            isMouseDown = true;
+            startX = e.pageX - forecastContainer.offsetLeft;
+            scrollLeft = forecastContainer.scrollLeft;
+        });
+
+        // mouse leave event stops slide
+        forecastContainer.addEventListener('mouseleave', () => {
+            isMouseDown = false;
+        });
+
+        // mouse up event stops slide
+        forecastContainer.addEventListener('mouseup', () => {
+            isMouseDown = false;
+        });
+
+        // handle mouse move event
+        forecastContainer.addEventListener('mousemove', (e) => {
+            if (!isMouseDown) 
+                return;
+            e.preventDefault();
+            const x = e.pageX - forecastContainer.offsetLeft;
+            const walk = (x - startX) * 2; // sliding sensitivity adjustment
+            forecastContainer.scrollLeft = scrollLeft - walk;
+        });
+
+        // create hourly forecast elements
+        function createHourlyForecastElements() {
+        forecastData.forEach((hour) => {
+            const hourDiv = document.createElement('div');
+            hourDiv.classList.add('hour-forecast');
+
+            // display houre data
+            const time = document.createElement('p');
+            time.textContent = `Time: ${hour.time}`;
+            hourDiv.appendChild(time);
+
+            const temperature = document.createElement('p');
+            temperature.textContent = `Temperature: ${hour.temp_c} °C`;
+            hourDiv.appendChild(temperature);
+
+            // might add pics also
+            const tempImg = document.createElement('img');
+            tempImg.src = `http://${hour.img.slice(2)}`
+            hourDiv.appendChild(tempImg);
+
+            forecastContainer.appendChild(hourDiv);
+        });
+        }
+
+        createHourlyForecastElements();
+
 
 
 })
