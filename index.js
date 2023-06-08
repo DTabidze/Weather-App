54bfa0a161ca53b46c48135715e6b7f5c081219f
Davit Tabidze <davidtabidzef1@gmail.com>
2023-06-07 22:15:12 -0400

    v1.8.1015

--- index.js
+++ index.js
@@ -37,12 +37,24 @@ document.addEventListener('DOMContentLoaded', async (event) => {
     try {
         const coordinates = await getLocation();
         console.log(coordinates[0], coordinates[1]);
-        //fetch (`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coordinates[0]}&longitude=${coordinates[1]}&localityLanguage=en`)
-        fetch (`https://geocode.xyz/${coordinates[0]},${coordinates[1]}?geoit=json&auth=111478610472165251572x86843`)
+        // fetch (`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coordinates[0]}&longitude=${coordinates[1]}&localityLanguage=en`)
+        // fetch (`https://api.opencagedata.com/geocode/v1/json?key=c3ae13d3bc664fe585db1e679751d31c&q=${coordinates[0]}%2C${coordinates[1]}&pretty=1`)
+        // fetch ('https://api.opencagedata.com/geocode/v1/json?key=c3ae13d3bc664fe585db1e679751d31c&q=40.7052011%2C-74.0141173&pretty=1')
+        // fetch (`https://geocode.xyz/${coordinates[0]},${coordinates[1]}?geoit=json&auth=111478610472165251572x86843`)
+        fetch (`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${coordinates[0]}&lon=${coordinates[1]}`)
         .then (response => response.json())
         .then (data => {
-            console.log(data)
-            creatingWeatherPanel(data.city);
+            console.log (data)
+            //console.log(data.results[0].components.city)
+            console.log (data.address.city)
+            let cityTown;
+            if (data.address.city != undefined) {
+                cityTown = data.address.city
+            } else if (data.address.town != undefined) {
+                cityTown = data.address.town
+            }
+
+            creatingWeatherPanel(cityTown)
             initAutocomplete();
 
             const searchIcon = document.getElementById('search-icon');
@@ -58,6 +70,13 @@ document.addEventListener('DOMContentLoaded', async (event) => {
                 creatingWeatherPanel(event.target['city-name'].value.split(',')[0])
                 updateCityList(document.getElementById("autocomplete-input").value)
             })
+
+            const tomorrowWeather = document.querySelector('button#Tomorrow');
+            tomorrowWeather.addEventListener ('click', (event) => displayWeatherTomorrow (document.querySelector('p#pLocation').textContent.split(',')[0]))
+
+            const todayWeather = document.querySelector('button#Today')
+            todayWeather.addEventListener ('click', (event) => creatingWeatherPanel (document.querySelector('p#pLocation').textContent.split(',')[0]))
+
             customCityLoad();
             
             const historyHeading = document.getElementById('history');
@@ -186,8 +205,7 @@ function createHourlyForecastElements(forecastData) {
     forecastData.forEach((hour) => {
         const hourDiv = document.createElement('div');
         hourDiv.classList.add('hour-forecast');
-        //hourDiv.style.marginLeft = 'auto'
-        // display houre data
+
         const time = document.createElement('p');
         time.textContent = `Time: ${hour.time}`;
         hourDiv.appendChild(time);
@@ -225,7 +243,6 @@ function creatingWeatherPanel (city) {
     //event.preventDefault();
     console.log(city);
     const URL = `http://api.weatherapi.com/v1/forecast.json?key=476fc45bade541f8988153529230506&q=${city}&days=7&aqi=no&alerts=no`
-
     fetch (URL)
     .then (response => response.json())
     .then (data => {
@@ -237,18 +254,14 @@ function creatingWeatherPanel (city) {
         const location = document.createElement('p');
         location.textContent = `${data.location.name}, ${data.location.region}, ${data.location.country}.`;
         location.classList.add('city-name');
+        location.id = 'pLocation'
         weatherDiv.appendChild(location);
 
         const currentTime = document.createElement('p');
         const currentTimeStr = `${data.location.localtime}`;
-        //currentTime.textContent = `${data.location.localtime}`;
-        //console.log(currentTime.textContent)
-
-        // const day = currentTimeStr.split('-')[2].slice(0,2);
-        // const time = currentTimeStr.split(' ')[1];
-        // const mnth = month[+currentTimeStr.split('-')[1]]
+ 
         currentTime.textContent = `${month[+currentTimeStr.split('-')[1]]} ${currentTimeStr.split('-')[2].slice(0,2)} ${currentTimeStr.split(' ')[1]}`
-
+        currentTime.id = 'pCurrentTime'
         currentTime.classList.add('temp-range');
         weatherDiv.appendChild(currentTime);
         
@@ -258,11 +271,13 @@ function creatingWeatherPanel (city) {
         const weatherImg = document.createElement('img');
         const img = data.current.condition.icon.slice(2);
         weatherImg.src = `http://${img}`;
+        weatherImg.id = 'weatherImg'
         weatherIconTemp.appendChild(weatherImg);
         
         const tempC = document.createElement('p');
         tempC.textContent = `${data.current.temp_c} °C`;
         tempC.classList.add('temperature');
+        tempC.id = 'pTemperature'
         weatherIconTemp.appendChild(tempC);
         
         weatherDiv.appendChild(weatherIconTemp);
@@ -270,16 +285,19 @@ function creatingWeatherPanel (city) {
         const weatherCondition = document.createElement('p');
         weatherCondition.textContent = data.current.condition.text;
         weatherCondition.classList.add('weather-condition');
+        weatherCondition.id = 'pWeatherCondition'
         weatherDiv.appendChild(weatherCondition);
         
         const tempMinMax = document.createElement('p');
         tempMinMax.textContent = `H: ${data.forecast.forecastday[0].day.maxtemp_c} °C | L: ${data.forecast.forecastday[0].day.mintemp_c} °C`;
         tempMinMax.classList.add('temp-range');
+        tempMinMax.id = 'pTempRange'
         weatherDiv.appendChild(tempMinMax);
         
         const windInfo = document.createElement('p');
         windInfo.textContent = `Wind: ${data.current.wind_kph} kph ${data.current.wind_dir}`;
         windInfo.classList.add('wind-info');
+        windInfo.id = 'pWindInfo'
         weatherDiv.appendChild(windInfo);
 
         const forecastData = houreForecastArr(data);
@@ -299,13 +317,43 @@ function initAutocomplete() {
   
     // Optional: Attach event listener for when a place is selected
     autocomplete.addListener('place_changed', onPlaceSelected);
-  }
+}
   
-  function onPlaceSelected() {
+function onPlaceSelected() {
     const place = this.getPlace();
     // Access the selected place object and perform further actions
     console.log(place);
-  }
+}
   
-  // Call the initAutocomplete function when the page finishes loading
-  //document.addEventListener('DOMContentLoaded', initAutocomplete);
+// Call the initAutocomplete function when the page finishes loading
+
+
+
+function displayWeatherTomorrow (city) {
+    //event.preventDefault();
+    console.log(city);
+    event.preventDefault();
+    const URL = `http://api.weatherapi.com/v1/forecast.json?key=476fc45bade541f8988153529230506&q=${city}&days=7&aqi=no&alerts=no`
+    fetch (URL)
+    .then (response => response.json())
+    .then (data => {
+        const currentTime = document.querySelector('p#pCurrentTime');
+        const currentTimeStr = `${data.forecast.forecastday[1].date}`;
+        currentTime.textContent = `${month[+currentTimeStr.split('-')[1]]} ${currentTimeStr.split('-')[2].slice(0,2)}`
+
+        const weatherImg = document.querySelector('img#weatherImg');
+        weatherImg.src = `http://${data.forecast.forecastday[1].day.condition.icon.slice(2)}`
+
+        const tempC = document.querySelector('p#pTemperature');
+        tempC.textContent = ''
+        
+        const weatherCondition = document.querySelector('p#pWeatherCondition');
+        weatherCondition.textContent = data.forecast.forecastday[1].day.condition.text;
+        
+        const tempMinMax = document.querySelector('p#pTempRange');
+        tempMinMax.textContent = `H: ${data.forecast.forecastday[1].day.maxtemp_c} °C | L: ${data.forecast.forecastday[1].day.mintemp_c} °C`;
+        
+        const windInfo = document.querySelector('p#pWindInfo');
+        windInfo.textContent = `Wind: ${data.forecast.forecastday[1].day.maxwind_kph} kph`;
+    })
+}
